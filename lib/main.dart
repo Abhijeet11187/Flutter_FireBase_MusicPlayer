d8a95cdc3582dd6f2223e75a7fe9721631473d39
Abhijeet11187 <lokhandeabhijeet11187@gmail.com>
2020-03-04 17:41:17 +0530

    Music Player with FireBase Streaming

--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,9 @@ import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 import 'package:flute_music_player/flute_music_player.dart';
 import 'package:flutter/services.dart';
+import 'package:dio/dio.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 
 void main() => runApp(MyApp());
 
@@ -30,42 +33,98 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  
   //Variables
 
-  List<Song> _songAll;
-  MusicFinder _musicFinder;
+  List _songAll=[
+    'Señorita Ringtone.mp3',
+    'Ringtone_mahadev.MP3',
+    'Ringtone Zeher.mp3',
+    'KGF_ringtone(128k).mp3',
+    'Thumka.mp3',
+    'Despacito_F..mp3',
+    'Heeriye.mp3',
+    'Hookah bar.mp3',
+    'Jigra Uri.mp3',
+    'Kadak_Ban_Emiway.mp3',
+    'Mungda mungda.mp3',
+    'Mulshi Pattern Arara.mp3',
+    'Namo_Namo_-_Kedarnath.mp3',
+    'Nazar Nazar (Hathyar).mp3',
+    'Ringtone Kaliyan.mp3',
+    'Sher_Aaya_Sher.mp3'
+    ];
   Directory dir;
   File testFile;
   int _currentSongIndex;
+  String songURL;
   Duration _currentPosition;
   ByteData bytes;
   AudioPlayer audioPlayer = new AudioPlayer();
-  bool _isMusicAvailable = false;
+  bool _isMusicAvailable = true;
 
   @override
   void initState() {
-    _getMusic();
+    // _getMusic();
     super.initState();
   }
 
-// ************ Playing Audio (Calling Function to audioPlayer) ************
 
-  Future _playLocal(String url) async {
-    if (url == null) {
+//  Playing Audio (Calling Function to audioPlayer) 
+
+  Future _playLocal(String songName) async {
+    if (songName == null) {
       print("Method is calling on Null");
     } else {
-      playMusic(url);
+      playMusic(songName);
       _music_Playing(context);
-      // final result = await _musicFinder.play(url, isLocal: true);
     }
   }
 
-// ************ Function to play Music using Plugin audioPlayer ************
+//  Loading Song From Firebase 
 
-  Future<void> playMusic(String kUrl) async {
-    print("In the Play Music");
+  static Future<dynamic> loadSongFromFirebase(BuildContext context, String songName)async{
+    return await FirebaseStorage.instance
+        .ref()
+        .child(songName)
+        .getDownloadURL();
+  }
 
-    await audioPlayer.play(kUrl);
+//   Downloading the Song 
+
+void downloadSong(BuildContext context)async{
+    Dio dio=Dio();
+    var dir=await getExternalStorageDirectory();
+    await dio.download(songURL, "${dir.path}/${_songAll[_currentSongIndex]}.mp3");
+    // Scaffold.of(context).showSnackBar(snackBar);  
+    print("Sneakbar is shown");
+}
+
+//  Snackbar
+
+    // final snackBar = SnackBar(
+    //   content: Text('Song Downloaded'),
+    //   action: SnackBarAction(
+    //     label: 'Undo',
+    //     onPressed: () {
+    //       // Some code to undo the change.
+    //     },
+    //   ),
+    // );
+
+
+//  Function to play Music using Plugin audioPlayer 
+
+  Future<void> playMusic(String songName) async {
+  
+    //  Getting Download URL from the Firebase 
+    await loadSongFromFirebase(context, songName)
+        .then((value) => {
+              print(value),
+              songURL = value,
+            });
+    //                  
+    await audioPlayer.play(songURL);
     audioPlayer.onAudioPositionChanged.listen((Duration p) {
       setState(() {
         _currentPosition = p;
@@ -74,19 +133,19 @@ class _MyHomePageState extends State<MyHomePage> {
     });
   }
 
-  // ************ Getting Music From Mobile Phone ************
+  //   Getting Music From Mobile Phone 
 
-  void _getMusic() async {
-    MusicFinder _musicFinder = new MusicFinder();
-    var songs = await MusicFinder.allSongs();
-    setState(() {
-      _songAll = songs;
-      _isMusicAvailable = true;
-    });
-  }
+  // void _getMusic() async {
+  //   MusicFinder _musicFinder = new MusicFinder();
+  //   var songs = await MusicFinder.allSongs();
+  //   setState(() {
+  //     _songAll = songs;
+  //     _isMusicAvailable = true;
+  //   });
+  // }
 
 
-  //Retruns Bottom Sheet While Music is running able to pause, resume,stop music
+  //   Retruns Bottom Sheet While Music is running able to pause, resume,stop music 
 
   Widget _music_Playing(context) {
     showModalBottomSheet(
@@ -97,10 +156,9 @@ class _MyHomePageState extends State<MyHomePage> {
             child: Column(
               children: <Widget>[
                 Container(
-                    height: 100, child: Image.asset("images/audioPlaying.gif")
-                    ),
+                    height: 100, child: Image.asset("images/audioPlaying.gif")),
                 Text(
-                  _songAll[_currentSongIndex].title,
+                  _songAll[_currentSongIndex],
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
                       fontSize: 17,
@@ -152,6 +210,13 @@ class _MyHomePageState extends State<MyHomePage> {
                         await audioPlayer.seek(_seekPosition);
                       },
                     ),
+                     IconButton(
+                      tooltip: 'Download Song',
+                      icon: Icon(Icons.file_download),
+                      onPressed: (){
+                        downloadSong(context);
+                      },
+                    ),
                   ],
                 )
               ],
@@ -174,16 +239,15 @@ class _MyHomePageState extends State<MyHomePage> {
                   return ListTile(
                     onTap: () => {
                       _currentSongIndex = index,
-                      _playLocal(_songAll[index].uri),
+                      _playLocal(_songAll[index]),
                     },
                     leading: CircleAvatar(
                       backgroundColor: Colors.orangeAccent,
                       child: Image.asset('images/MusicIcon.png'),
                     ),
-                    title: Text(_songAll[index].title),
+                    title: Text(_songAll[index]),
                   );
                 })
-            : Center(child: Image.asset('images/waitPlease.gif'))
-            );
+            : Center(child: Image.asset('images/waitPlease.gif')));
   }
 }
